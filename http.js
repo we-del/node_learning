abae3039cd5820c796c5f552fbbd73841c3b172a
xxx <1103644163@qq.com>
2022-03-19 18:22:22 +0800

    http简单实现

---
+++ http.js
@@ -0,0 +1,22 @@
+// 我来创建一个http请求
+
+class OpenHttp {
+    constructor() {
+        this.http = require("http");
+        this.server = null;
+        this.qs = require("querystring");
+    }
+
+    start() {
+        this.server = this.http.createServer((req, res) => {
+            let msg = req.url.split('?')[1];
+            console.log("msg" + msg);
+            console.log("t ", (this.qs.parse(msg)));
+            res.setHeader('content-type', 'text/html;charset=utf8');
+            res.end("<h1>我来返回数据</h1>>");
+        });
+        this.server.listen(5000, () => console.log("端口监听中"));
+    }
+}
+
+new OpenHttp().start();
